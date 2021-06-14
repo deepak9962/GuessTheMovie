855daad78abf16085c6f979831ba249a7d3ee0c5
deepak9962 <dk0137771@gmail.com>
2021-06-14 16:23:59 +0530

    First time done

---
+++ src/Game.java
@@ -0,0 +1,181 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class Game {
+
+    private static final GuessTheMovie guessTheMovie = new GuessTheMovie();
+
+    // String array for daces
+    private final String[] daces = new String[guessTheMovie.getMovie().length()];
+
+    // An empty dace
+    private final String emptyDace = "_";
+
+    // Scanner to get the text from console
+    private final Scanner scanner = new Scanner(System.in);
+
+    // check whether the user won or loose, Default: false
+    private boolean hasWon = false;
+
+    /**
+     * This is where the actual game process starts and this function gets the letters
+     * from users and replaces with the daces to reveal a movie letter.
+     * <p>
+     * if the letter is right it according to the movie it selected randomly it will
+     * reveal that movie's letter position and repeats the same process using loops.
+     * <p>
+     * if the letter is wrong then user looses he guess(es) points and end up loosing
+     * if wrong letters given until the guess(es) left.
+     * <p>
+     * Total guess(es) 10
+     * <p>
+     * If user gets the right letter, guess(es) points will not decrease but if user
+     * gets the wrong letter, guess(es) points will decrease by 1.
+     * <p>
+     * If user guess the write movie, he wins.
+     * If user is not able to guess, he looses.
+     */
+    public void startGame() {
+
+        // creates loop for empty daces to get the exact quantity of daces as per movie has letters
+        for (int i = 0; i < guessTheMovie.getMovie().length(); i++) {
+
+            // Add the spaces if movie has any
+            for (int j = 0; j < guessTheMovie.getMovie().length(); j++) {
+                if (j == guessTheMovie.getMovie().indexOf(" ", j)) {
+                    daces[j] = " ";
+                }
+            }
+
+            daces[i] = emptyDace;
+        }
+
+        // prints out the daces on screen
+        System.out.println(createString());
+
+        // total guess(es) point
+        int count = 10;
+
+        // loop to get the letter from user and check whether its right or not,
+        // and if the letter is right then replace the letter with dace on it's position
+        // or if the letter is wrong then decreases the guess(es) points.
+        for (int i = 0; i <= guessTheMovie.getMovie().length(); i++) {
+
+            // prints how many guess(es) you have or left
+            System.out.println("You have " + count + " guess(es)");
+
+            // Get's the letters from user
+            String guess = scanner.next();
+
+            // check if the user entered multiple characters.
+            if (guess.length() == 1) {
+
+                // check the letter whether it is the right or wrong
+                // and if the letter is right then replace the letter with dace on it's position.
+                for (int j = 0; j < guessTheMovie.getMovie().length(); j++) {
+
+                    // searches and compares each letter one by one of the movie and finds the index of
+                    // the letter if the user provided letter matches and replaces the letter.
+                    if (j == guessTheMovie.getMovie().indexOf(guess) && daces[j].contains(emptyDace)) {
+                        daces[j] = guess;
+
+                        // checks if the movie is guessed or if there is no empty space left.
+                        checkWinner();
+
+                        // increase the guess(es) by one if guess is write because below down it
+                        // is going to decrease, so we don't want users to loose points for guessing it right.
+                        count++;
+                    } else if (j == guessTheMovie.getMovie().indexOf(guess, j) && daces[j].contains(emptyDace)) {
+                        daces[j] = guess;
+                        checkWinner();
+                    } else {
+
+                        // since we have exact number of loop as equal to movie has letters.
+                        // it checks whether the letter is guessed already and if it is guessed
+                        // already then decrease the loop by one, so that the loop keeps continues.
+                        if (daces[j].contains(guess)) {
+                            System.out.println("You already guessed it! Please try different letter!");
+                            i--;
+                        }
+                    }
+                }
+
+                // Shows the updated string inside the loop
+                System.out.println(createString());
+
+                // if the guess is wrong this statement prints and
+                // decrease the loop by one, so that the loop keeps continues.
+                if (!(guessTheMovie.getMovie().contains(guess))) {
+                    System.out.println("Good guess, Try again!");
+                    i--;
+                }
+
+                // If guessed wrong then guess(es) point will decrease
+                count--;
+
+                // This method checks if the user has won, prints the won statement
+                // or if the user looses then loose statement prints.
+                if (hasWon) {
+                    System.out.println("WOW CONGRATULATIONS YOU WON!" +
+                            "\n" +
+                            "You guessed the right movie!");
+                    System.out.println("The movie was : " + guessTheMovie.getMovie());
+                    return;
+                } else if (count == 0) {
+                    System.out.println("You used all your guess(es) and not get the movie\n" +
+                            "YOU LOOSE!");
+                    System.out.println("The movie was : " + guessTheMovie.getMovie());
+                    return;
+                }
+            } else {
+                System.out.println("Please enter one letter at a time.");
+                i--;
+            }
+        }
+    }
+
+    /**
+     * StringBuilder creates and simple looking dace string hiding the movie
+     * name and having the same length as movie has.
+     *
+     * @return a simple looking string instead of an array with commasZ
+     */
+    private @NotNull
+    StringBuilder createString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // looping through the length of movie and appending the daces
+        // and forming a one simple movie name, without commas
+        for (int i = 0; i < guessTheMovie.getMovie().length(); i++) {
+            stringBuilder.append(daces[i]);
+        }
+        return stringBuilder;
+    }
+
+    /**
+     * This function checks whether the user won or not.
+     */
+    private void checkWinner() {
+
+        // Getting the length of movie
+        int count = guessTheMovie.getMovie().length();
+
+        // Creating a loop to check if the user guessed the movie or
+        // it still empty daces has left.
+        // If there is still empty daces left then don't decrease the count
+        // of this method (which is movie length) or if there is no
+        // empty daces left then set the count to 0 to declare winner
+        for (int i = 0; i < guessTheMovie.getMovie().length(); i++) {
+            if (!(daces[i].contains(emptyDace))) {
+                count--;
+            }
+        }
+
+        // if there is no emptyDaces which means user won then variable count becomes 0 and
+        // sets the boolean variable hasWon to true
+        if (count == 0) {
+            hasWon = true;
+        }
+    }
+}
---
+++ src/GuessTheMovie.java
@@ -0,0 +1,91 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GuessTheMovie {
+
+    private static final Game game = new Game();
+    private final String movie = searchMovie(); // Getting a random movie
+
+    public static void main(String[] args) {
+
+        // Starting GuessTheMovie game;
+        System.out.println("Hello there!\nWe have selected you a random movie name, Try to guess the movie;");
+        System.out.println("Enter a letter to guess the movie: ");
+
+        // Calling startGame function from Game.java
+        game.startGame();
+
+    }
+
+    /**
+     * Getting a random movie name from the file where many movies name are stored.
+     * Scanner scans the file and split create an array of movies and select one random movie.
+     *
+     * @return a random selected movie
+     */
+    private String searchMovie() {
+
+        // ArrayList for string movie
+        List<String> itemMovie = new ArrayList<>();
+
+        // File to get the movie file list
+        File file = new File("assets/MovieList");
+
+        // Trying try/catch block in case of if the file is missing or some error with the file
+        Scanner scanner = null;
+        try {
+
+            // attach the file to the scanner
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // String line to get each movie available in movie list file
+        String str_line;
+
+        // Looping until no more lines
+        while (true) {
+            assert scanner != null;
+            if (!scanner.hasNextLine()) break;
+            str_line = scanner.nextLine();
+            str_line = str_line.trim();
+            if (str_line.length() != 0) {
+
+                // Creating a list of movies
+                itemMovie.add(str_line);
+            }
+        }
+
+        // Adding list of movies each in an String array
+        String[] movieList = itemMovie.toArray(new String[0]);
+
+        // getting the size of array
+        int length = movieList.length;
+
+        // Returning a selected random movie
+        return movieList[position(length)];
+    }
+
+    /**
+     * Position to get a random number to select a random movie
+     *
+     * @param length considered as size of array
+     * @return a random number to get a random movie
+     */
+    private static int position(int length) {
+        return (int) ((Math.random() * length) - 1);
+    }
+
+    /**
+     * This function is get called for the movie name.
+     *
+     * @return movie name
+     */
+    public String getMovie() {
+        return movie;
+    }
+}
